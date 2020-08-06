017ae9455d16f3e344125c5d1003dddcc71caab7
kushal agrawal <kagrawal61@gmail.com>
2020-08-06 14:28:12 +0530

    faq-component-added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
+import Faq from 'react-faq-component';
 import './App.css';
+
+const data = {
+  title: "FAQ For Placements and Higher Studies (PES)",
+  rows: [
+    {
+      title: "Lorem ipsum dolor sit amet,",
+      content: "Lorem ipsum dolor sit amet, consectetur "
+    },
+    {
+      title: "Nunc maximus, magna at ultricies elementum",
+      content: "Nunc maximus, magna at ultricies elementum, risus turpis vulputate quam."
+    },
+    {
+      title: "Curabitur laoreet, mauris vel blandit fringilla",
+      content: "Curabitur laoreet, mauris vel blandit fringilla, leo elit rhoncus nunc"
+    },
+    {
+      title: "What is the package version",
+      content: "v1.0.5"
+    }]
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div>
+        <Faq data={data}/>
+      </div>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +43,10 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <div>
+        <h3>For any queries contact us here</h3>
+      </div>
     </div>
   );
 }
